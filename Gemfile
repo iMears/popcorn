37d00f84c3a687b2947567d0adf328f2951c35e5
imears <maxwellmears@gmail.com>
2016-01-30 11:24:16 -0800

    Add weather report

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'twilio-ruby', '~> 4.2.1'
 gem 'responders', '~> 2.0'
+gem 'open-weather'
+gem 'dotenv-rails', :require => 'dotenv/rails-now'
+gem 'awesome_print'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,29 @@
+require 'open_weather'
+
 class WelcomeController < ApplicationController
   def index
+    puts '-' * 10
+    ap params
+    puts '-' * 10
+    options = { units: "imperial", APPID: ENV["API_KEY"] }
+    weather_report = OpenWeather::Current.city("#{params["FromCity"]}, #{params["FromState"]}", options)
+    ap weather_report
+    weather_description = weather_report["weather"][0]["description"]
+    temp_min = weather_report["main"]["temp_min"]
+    temp_max  = weather_report["main"]["temp_max"]
+    ap weather_report
+
+    user = User.where(phone: params["Caller"]).first
+
+    pacific_time = Time.now.dst? ? 'pacific daylight time' : 'pacific standard time'
 
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Hello, the time is #{Time.now.strftime("%I:%M:%S %P")}", voice: 'alice'
+      if user
+        r.Say "Hello #{user.name}, the current time is #{Time.now.strftime("%I:%M:%S %P")} #{pacific_time}.", voice: 'alice'
+      else
+        r.Say "Hello, the current time is #{Time.now.strftime("%I:%M:%S %P")} #{pacific_time}.", voice: 'alice'
+      end
+      r.Say "The current weather report for #{params["FromCity"]} is #{weather_description}, with a high temperature of #{temp_max} and a low of #{temp_min} degrees Fahrenheit.", voice: 'alice'
     end
 
     render xml: response.text
